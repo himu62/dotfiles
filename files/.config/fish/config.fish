460fb69ba145b28833d136117ca3e332de3876ea
himu62 <hiromu_kobayashi@voyagegroup.com>
2017-05-01 18:33:59 +0900

    add libressl path

--- files/.config/fish/config.fish
+++ files/.config/fish/config.fish
@@ -3,4 +3,6 @@
 set -x PATH /usr/local/opt/coreutils/libexec/gnubin $PATH
 set -x MANPATH /usr/local/opt/coreutils/libexec/gnuman $MANPATH
 
+set -x PATH /usr/local/opt/libressl/bin $PATH
+
 set -x PATH ~/bin $PATH
