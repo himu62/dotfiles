1d7bcc12d308a801d4c1b4779e979e83cef0b06e
himu62 <hiromu_kobayashi@voyagegroup.com>
2017-05-02 10:05:17 +0900

    add fish config

--- files/.config/fish/config.fish
+++ files/.config/fish/config.fish
@@ -1,4 +1,9 @@
-
+function fish_prompt
+  set_color green
+  echo -n "~"
+  set_color normal
+  echo -n ">"
+end
 
 set -x PATH /usr/local/opt/coreutils/libexec/gnubin $PATH
 set -x MANPATH /usr/local/opt/coreutils/libexec/gnuman $MANPATH
