87fbea289f1ce936d2cc4f24308fb20ba9e96ac9
himu62 <hiromu_kobayashi@voyagegroup.com>
2017-08-21 12:00:00 +0900

    もろもろ改善

--- files/.config/fish/config.fish
+++ files/.config/fish/config.fish
@@ -5,14 +5,18 @@ function fish_prompt
   echo -n "> "
 end
 
-set -U fish_user_paths /usr/local/bin
-set -U fish_user_paths /usr/local/sbin $fish_user_paths
-set -U fish_user_paths /usr/local/opt/coreutils/libexec/gnubin $fish_user_paths
+set -x PATH $PATH /usr/local/bin
+set -x PATH $PATH /usr/local/sbin
+set -x PATH $PATH /usr/local/opt/coreutils/libexec/gnubin
 set -x MANPATH /usr/local/opt/coreutils/libexec/gnuman $MANPATH
 
-set -U fish_user_paths /usr/local/opt/libressl/bin $fish_user_paths
-set -U fish_user_paths ~/bin $fish_user_paths
-set -U fish_user_paths ~/.nodebrew/current/bin $fish_user_paths
+set -x PATH $PATH /usr/local/opt/libressl/bin
+set -x PATH $PATH ~/bin
+set -x PATH $PATH ~/.nodebrew/current/bin
+
+set -x PATH $PATH ~/Library/Android/sdk/platform-tools
+set -x PATH $PATH ~/Library/Android/sdk/tools
+set -x ANDROID_HOME ~/Library/Android/sdk $PATH
 
 set -x GOPATH ~
 
@@ -20,11 +24,18 @@ alias gst="git status"
 alias gci="git commit -m"
 alias glog="git log --oneline --graph"
 alias gdf="git diff HEAD"
+alias gpl="git pull origin (~/dotfiles/bin/echo-git-branch)"
+alias gps="git push origin (~/dotfiles/bin/echo-git-branch)"
 alias ll="ls -hl"
 alias la="ls -ahl"
 
-# The next line updates PATH for the Google Cloud SDK.
-if [ -f '/Users/h-kobayashi/google-cloud-sdk/path.fish.inc' ]; if type source > /dev/null; source '/Users/h-kobayashi/google-cloud-sdk/path.fish.inc'; else; . '/Users/h-kobayashi/google-cloud-sdk/path.fish.inc'; end; end
+#rbenv init - | source
+set -x PATH $PATH ~/.rbenv/shims
 
-# rbenv init - | source
-set -U fish_user_paths ~/.rbenv/shims $fish_user_paths
+# The next line updates PATH for the Google Cloud SDK.
+#if [ -f '/Users/h-kobayashi/google-cloud-sdk/path.fish.inc' ]; if type source > /dev/null; source '/Users/h-kobayashi/google-cloud-sdk/path.fish.inc'; else; . '/Users/h-kobayashi/google-cloud-sdk/path.fish.inc'; end; end
+
+#plenv init - | source
+set -x PATH $PATH ~/.plenv/shims
+
+set -x PATH $PATH ~/.cargo/bin
